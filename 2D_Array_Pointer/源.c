5c541b7d552e2e47c17cd79946893f626eeb4f2f
han <1402469615@qq.com>
2017-04-22 09:52:48 +0800

    更新源代码

--- 2D_Array_Pointer/源.c
+++ 2D_Array_Pointer/源.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 int main()
 {
+	printf("int的长度为: %d \n",sizeof(int));
 	int a[3][4] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 	int *p, m;
 	for (m = 1, p = &a[0][0]; p <= &a[2][3]; p++)
 	{
-		printf("%4d", *p);
+		printf("%8d", *p);
 		if ((m++) % 4 == 0)
 			printf("\n");
 	}
 	int b[3][4] = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 	int(*q)[4], i, j;
-	q = a;
+	q = b;
 	for (i = 1; i <= 3; i++)
 	{
 		for (j = 1; j <= 4; j++)
-			printf("%d ", *(*(q + i - 1) + j - 1));
+		{
+			printf("%8d", *(*(q + i - 1) + j - 1));//打印元素
+		}
+		printf("\n");
+		for (j = 1; j <= 4; j++)
+		{
+			printf("%8d", *(q + i - 1));//打印每行首地址
+		}
+		printf("\n");
+		for (j = 1; j <= 4; j++)
+		{
+			printf("%8d", *(q + i - 1)+j-1);//打印每行的元素地址
+		}
 		printf("\n");
 	}
 }
